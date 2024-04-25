d533666023858b4e7cbafbd0ffb86eae6ddeedd1
IRFAN <irfanhpssain@gmail.com>
2024-04-26 00:44:14 +0600

    background added

--- app/src/main/java/com/example/diploma_project/activity/AddressActivity.kt
+++ app/src/main/java/com/example/diploma_project/activity/AddressActivity.kt
@@ -28,55 +28,55 @@ class AddressActivity : AppCompatActivity() {
 
             startActivity(Intent(this,CheckoutActivity::class.java))
 
-//            validateData(
-//                binding.userName.text.toString(),
-//                binding.userPhoneNumber.text.toString(),
-//                binding.division.text.toString(),
-//                binding.city.text.toString(),
-//                binding.address.text.toString(),
-//                binding.postCode.text.toString()
-//            )
+            validateData(
+                binding.userName.text.toString(),
+                binding.userPhoneNumber.text.toString(),
+                binding.division.text.toString(),
+                binding.city.text.toString(),
+                binding.address.text.toString(),
+                binding.postCode.text.toString()
+            )
         }
 
     }
 
-//    private fun validateData(name: String, number: String, division: String,
-//                             city: String, address: String, postCode: String) {
-//
-//        if (name.isEmpty() || number.isEmpty() || division.isEmpty() ||
-//            city.isEmpty() || address.isEmpty() || postCode.isEmpty()){
-//
-//
-//            Toast.makeText(this,"Please fill all details",Toast.LENGTH_SHORT).show()
-//        }else{
-//            storeData(name,number,division,city,address,postCode)
-//        }
-//    }
+    private fun validateData(name: String, number: String, division: String,
+                             city: String, address: String, postCode: String) {
 
-//    private fun storeData(name: String, number: String, division: String, city: String, address: String, postCode: String) {
-//
-//        val map = hashMapOf<String, Any>()
-//        map["division"] = division
-//        map["city"] = city
-//        map["address"] = address
-//        map["postCode"] = postCode
-//        map["userName"] = name
-//        map["userPhoneNumber"] = number
-//
-//        Firebase.firestore.collection("users")
-//            .document(preferences.getString("userPhoneNumber", "")!!)
-//            .update(map).addOnSuccessListener {
-//                startActivity(Intent(this,CheckoutActivity::class.java))
-//            }
-//            .addOnFailureListener {
-//                Toast.makeText(this, "Something went wrong",Toast.LENGTH_SHORT).show()
-//            }
-//    }
+        if (name.isEmpty() || number.isEmpty() || division.isEmpty() ||
+            city.isEmpty() || address.isEmpty() || postCode.isEmpty()){
+
+
+            Toast.makeText(this,"Please fill all details",Toast.LENGTH_SHORT).show()
+        }else{
+            storeData(name,number,division,city,address,postCode)
+        }
+    }
+
+    private fun storeData(name: String, number: String, division: String, city: String, address: String, postCode: String) {
+
+        val map = hashMapOf<String, Any>()
+        map["division"] = division
+        map["city"] = city
+        map["address"] = address
+        map["postCode"] = postCode
+        map["userName"] = name
+        map["userPhoneNumber"] = number
+
+        Firebase.firestore.collection("users")
+            .document(preferences.getString("userPhoneNumber", "1")!!)
+            .update(map).addOnSuccessListener {
+                startActivity(Intent(this,CheckoutActivity::class.java))
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Something went wrong",Toast.LENGTH_SHORT).show()
+            }
+    }
 
 
     private fun loadUserInfo() {
         Firebase.firestore.collection("user")
-            .document(preferences.getString("userPhoneNumber", "")!!)
+            .document(preferences.getString("userPhoneNumber", "1")!!)
             .get().addOnSuccessListener {
                 binding.userName.setText(it.getString("userName"))
                 binding.division.setText(it.getString("division"))
