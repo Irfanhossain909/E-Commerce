809fc700b62308f8009d7a9256d7d068ab757dd1
IRFAN <irfanhpssain@gmail.com>
2024-04-27 02:38:37 +0600

    all background added and tested

--- app/src/main/java/com/example/diploma_project/activity/CheckoutActivity.kt
+++ app/src/main/java/com/example/diploma_project/activity/CheckoutActivity.kt
@@ -1,21 +1,32 @@
 package com.example.diploma_project.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.diploma_project.LoginActivity
+import com.example.diploma_project.MainActivity
 import com.example.diploma_project.R
+import com.example.diploma_project.databinding.ActivityCheckoutBinding
 
 class CheckoutActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCheckoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_checkout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityCheckoutBinding.inflate(layoutInflater)
+
+
+        binding.textView16.setOnClickListener {
+            var intent = Intent(this@CheckoutActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+        setContentView(binding.root)
+
+
+
     }
 }
